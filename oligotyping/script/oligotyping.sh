550d3a14332b3b5848870581bbcdd1333b8d8fad
Guangyu Li <li.gua@husky.neu.edu>
2020-07-23 17:35:27 -0400

    updated oligotyping position filtering and inputj

--- oligotyping/script/oligotyping.sh
+++ oligotyping/script/oligotyping.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 n_threads=4
-msa=65
-n_pos=7
+msa=65 # consult the msa_test output
 aln="mothur2oligo.fasta"
+positions=$(cat $aln".oligo_pos.custom.txt")
 
 # oligotyping needs python2
 module load python/2.7.15
@@ -12,13 +12,10 @@ module load python/2.7.15
 module load R/3.6.1
 
 entropy=$aln"-ENTROPY"
-out_dir=$aln".position_oligotype"
-# find the positions
-positions=$(sort -nrk2 $entropy | head -n $n_pos | cut -f1 |\
-	perl -ne 'chomp; print($_, ",");' | sed 's/,$//')
-echo $positions
-oligotype -M $msa -s 3 -C $positions -N $n_threads -o $out_dir \
-	$aln $aln"-ENTROPY"
+out_dir=$aln".oligotype."$positions
+oligotype -M $msa -s 3 -N $n_threads -o $out_dir \
+	-C $(echo $positions | sed 's/[_.]/,/g') \
+	$aln $entropy
 
 module unload R/3.6.1
 deactivate
