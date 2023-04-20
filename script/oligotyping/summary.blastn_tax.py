30b6ecdc0b489cd1dd01e7e6633eb33976254b00
Guangyu Li <gl343@cornell.edu>
2023-04-20 19:38:55 -0400

    added table output for blastn tax summary

--- script/oligotyping/summary.blastn_tax.py
+++ script/oligotyping/summary.blastn_tax.py
@@ -2,6 +2,7 @@
 
 import argparse
 import collections
+import io
 import itertools
 import matplotlib
 import matplotlib.pyplot
@@ -24,7 +25,7 @@ def get_args():
 			"extension (exclusive with -x/--scan-ext)")
 	ap.add_argument("-d", "--delimiter", type=str, default="\t",
 		metavar="str",
-		help="delimiter in input files [<tab>]")
+		help="delimiter in input/output tables [<tab>]")
 	ap.add_argument("-k", "--key-field", type=int, default=2,
 		metavar="int",
 		help="the taxnomy value field number (0-based) in input files [0]")
@@ -32,16 +33,32 @@ def get_args():
 		metavar="int",
 		help="number of taxons to show in legend, increase this number too much"
 			" will cause inevitable color confliction [20]")
-	ap.add_argument("-p", "--plot", type=str, default="-",
+	ap.add_argument("-t", "--table", type=str, default="-",
+		metavar="file",
+		help="output table [stdout]")
+	ap.add_argument("-p", "--plot", type=str,
 		metavar="png",
-		help="output plot image, or write to stdout by default '-'")
+		help="draw a stackbar image plot only if an output file set")
+
 	# parse and refine arsg
 	args = ap.parse_args()
 	if (args.scan_ext is None) and (args.file_list is None):
 		args.scan_ext = "blastdbcmd"
-	if args.plot == "-":
-		args.plot = sys.stdout.buffer
+	if args.table == "-":
+		args.table = sys.stdout
+
 	return args
+
+
+def get_fp(f, *ka, factory=open, **kw) -> io.IOBase:
+	if isinstance(f, io.IOBase):
+		ret = f
+	elif isinstance(f, str):
+		ret = factory(f, *ka, **kw)
+	else:
+		raise TypeError("first argument of get_fp() must be str or io.IOBase, "
+			"got '%s'" % type(f).__name__)
+	return ret
 
 
 def read_oligo_tax_count(fname, key_field, *, delimiter="\t") \
@@ -86,39 +103,55 @@ def read_oligo_tax_count_in_dir(dirname, *, scan_ext=None, file_list=None,
 	return ret
 
 
+def write_output_table(f, oligo_tax: dict, *, delimiter="\t") -> None:
+	# get unique taxons from Counter's keys
+	# get sorted as in list
+	oligos = sorted(oligo_tax.keys())
+	uniq_tax = sorted(set(itertools.chain(*oligo_tax.values())))
+	total_counts = {k: v.total() for k, v in oligo_tax.items()}
+	with get_fp(f, "w") as fp:
+		header_line = delimiter.join([""] + ["OPU_%03u" % i for i in oligos])
+		print(header_line, file=fp)
+		for t in uniq_tax:
+			vals = [oligo_tax[i][t] / total_counts[i] for i in oligos]
+			line = delimiter.join([t] + [str(i) for i in vals])
+			print(line, file=fp)
+	return
+
+
 def setup_layout(figure, n_oligo):
 	layout = dict()
 	layout["figure"] = figure
 
 	# margins
-	left_margin_inch	= 0.3
-	right_margin_inch	= 2.3
-	top_margin_inch		= 0.1
-	bottom_margin_inch	= 1.5
+	left_margin_inch = 0.3
+	right_margin_inch = 2.3
+	top_margin_inch = 0.1
+	bottom_margin_inch = 1.5
 
 	# bar width
-	bar_width_inch		= 0.2
-	bar_height_inch		= 3.0
-	axes_width_inch		= bar_width_inch * n_oligo
-	axes_height_inch	= bar_height_inch
+	bar_width_inch = 0.2
+	bar_height_inch = 3.0
+	axes_width_inch = bar_width_inch * n_oligo
+	axes_height_inch = bar_height_inch
 
 	# figure dimensions
-	figure_width_inch	= left_margin_inch + axes_width_inch + right_margin_inch
-	figure_height_inch	= top_margin_inch + axes_height_inch + bottom_margin_inch
+	figure_width_inch = left_margin_inch + axes_width_inch + right_margin_inch
+	figure_height_inch = top_margin_inch + axes_height_inch + bottom_margin_inch
 	figure.set_size_inches(figure_width_inch, figure_height_inch)
 
 	# create axes
-	axes_left	= left_margin_inch / figure_width_inch
-	axes_bottom	= bottom_margin_inch / figure_height_inch
-	axes_width	= axes_width_inch / figure_width_inch
-	axes_height	= axes_height_inch / figure_height_inch
+	axes_left = left_margin_inch / figure_width_inch
+	axes_bottom = bottom_margin_inch / figure_height_inch
+	axes_width = axes_width_inch / figure_width_inch
+	axes_height = axes_height_inch / figure_height_inch
 	axes = figure.add_axes([axes_left, axes_bottom, axes_width, axes_height])
 	layout["axes"] = axes
 	# axes style
-	#for sp in axes.spines.values():
-	#	sp.set_visible(False)
+	# for sp in axes.spines.values():
+	# sp.set_visible(False)
 	axes.set_facecolor("#E8E8F8")
-	axes.tick_params(left = False, labelleft = False, bottom = False)
+	axes.tick_params(left=False, labelleft=False, bottom=False)
 
 	return layout
 
@@ -185,6 +218,9 @@ class LegendColors(list):
 
 
 def plot_oligo_tax_stackbar(png, oligo_tax: dict, num_legend_taxons: int = 20):
+	if png is None:
+		return
+
 	u_oligo, u_tax, bs_mat = get_oligo_tax_bootstrap_matrix(oligo_tax)
 
 	n_oligo, n_tax = len(u_oligo), len(u_tax)
@@ -197,30 +233,30 @@ def plot_oligo_tax_stackbar(png, oligo_tax: dict, num_legend_taxons: int = 20):
 	# plot
 	colors = LegendColors()
 	axes = layout["axes"]
-	bottom = numpy.zeros(n_oligo, dtype = float)
+	bottom = numpy.zeros(n_oligo, dtype=float)
 	handles = list()
 	x = numpy.arange(n_oligo) + 0.5
 	for h, t, c in zip(bs_mat.T, u_tax, colors):
 		if len(handles) >= num_legend_taxons:
 			break
-		bar = axes.bar(x, h, width = 0.8, bottom = bottom,
-			align = "center", edgecolor = "#404040", linewidth = 0.5,
-			facecolor = c, label = t)
+		bar = axes.bar(x, h, width=0.8, bottom=bottom,
+			align="center", edgecolor="#404040", linewidth=0.5,
+			facecolor=c, label=t)
 		bottom += h
 		handles.append(bar)
 
 	# check if 'others' needs to be added
 	if len(handles) < n_tax:
 		h = bs_mat[:, len(handles):].sum(axis=1)
-		bar = axes.bar(x, h, width = 0.8, bottom = bottom,
-			align = "center", edgecolor = "#404040", linewidth = 0.5,
-			facecolor = "#ffffff", label = "others")
+		bar = axes.bar(x, h, width=0.8, bottom=bottom,
+			align="center", edgecolor="#404040", linewidth=0.5,
+			facecolor="#ffffff", label="others")
 		bottom += h
 		handles.append(bar)
 
 	# legend
-	axes.legend(handles = handles, loc = 2, fontsize = 8,
-		bbox_to_anchor = (1.002, 1.02))
+	axes.legend(handles=handles, loc=2, fontsize=8,
+		bbox_to_anchor=(1.002, 1.02))
 
 	# axis misc
 	axes.set_xlim(0, n_oligo)
@@ -228,10 +264,10 @@ def plot_oligo_tax_stackbar(png, oligo_tax: dict, num_legend_taxons: int = 20):
 	axes.set_ylabel("Taxonomy bootstrap")
 	axes.set_xticks(x)
 	axes.set_xticklabels(["OLIGO_%03u" % i for i in u_oligo],
-		rotation = 90, fontsize = 8,
-		horizontalalignment = "center", verticalalignment = "top")
+		rotation=90, fontsize=8,
+		horizontalalignment="center", verticalalignment="top")
 
-	matplotlib.pyplot.savefig(png, dpi = 300)
+	matplotlib.pyplot.savefig(png, dpi=300)
 	matplotlib.pyplot.close()
 	return
 
@@ -242,7 +278,9 @@ def main():
 	oligo_tax = read_oligo_tax_count_in_dir(args.dirname,
 		scan_ext=args.scan_ext, file_list=args.file_list,
 		key_field=args.key_field, delimiter=args.delimiter)
-	# plot
+	# table output
+	write_output_table(args.table, oligo_tax, delimiter=args.delimiter)
+	# plot output
 	plot_oligo_tax_stackbar(args.plot, oligo_tax,
 		num_legend_taxons=args.num_legend_taxons)
 	return
